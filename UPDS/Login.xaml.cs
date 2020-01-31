7f7d1b2ec0ab181815c85cd4924be264b147f01c
張淵博 <zhang.yuanbo@nisshin-sci.co.jp>
2020-01-31 19:27:17 +0900

    login

---
+++ UPDS/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using Service;
+
+namespace UPDS {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window {
+        public Login () {
+            InitializeComponent();
+            this.Loaded += ( obj, env ) => {
+                init();
+            };
+        }
+
+        private TestService testService = new TestService();
+        private void init () {
+            registerClickEvent();
+        }
+
+        private void registerClickEvent () {
+            this.btn_Login.Click += Btn_Login_Click;
+            this.btn_Cancel.Click += Btn_Cancel_Click;
+        }
+
+        private void Btn_Cancel_Click ( object sender, RoutedEventArgs e ) {
+            this.Close();
+        }
+
+        private void Btn_Login_Click ( object sender, RoutedEventArgs e ) {
+            bool right = true;
+            if ( right ) {
+                // 进入主界面
+            } else {
+
+            }
+        }
+
+        public string test () {
+            string str = testService.TestFunction();
+            //MessageBox.Show(str);
+            return str;
+        }
+    }
+}
